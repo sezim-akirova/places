20ea5e3e98f89c4ae8f3a1ee74828f8b24b736f3
Akirova Sezim <112478706+sezim-akirova@users.noreply.github.com>
2023-05-16 14:04:05 +0300

    text

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,10 +11,19 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      body: Center(
-        child: Text("Hello!"),
-     )
-    );
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, //прозрачный фон
+        elevation: 0, // убираем тени
+        title: const Text(
+          "Список\nинтересных мест", //обратный слеш + n = enter в тексте
+          textAlign: TextAlign.left, // текст по левому краю
+          style: TextStyle(
+            fontSize: 32,
+            color: Color.fromRGBO(37, 40, 73, 1), // цвет текста по дизайну
+          ),
+      ),// заголовок
+      toolbarHeight: 100, // задаем высоту апп бару
+    ),
+   );
   }
 }
