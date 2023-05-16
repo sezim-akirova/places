7018b95dda71edc29115a23c380abc6abda76c4b
Akirova Sezim <112478706+sezim-akirova@users.noreply.github.com>
2023-05-16 18:23:51 +0300

    richText

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -13,17 +13,43 @@ class _SightListScreenState extends State<SightListScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent, //прозрачный фон
-        elevation: 0, // убираем тени
-        title: const Text(
-          "Список\nинтересных мест", //обратный слеш + n = enter в тексте
+        elevation: 0, // убираем тени 
+        toolbarHeight: 100, // задаем высоту апп бару
+        title: RichText(
+          text: const TextSpan(
+            text: "С",
+            style: TextStyle(
+               fontSize: 32,
+               color: Color.fromRGBO(76, 175, 80, 1),
+            ),
+            children: [
+             TextSpan(
+               text: "писок\n",
+               style: TextStyle(
+                 fontSize: 32,
+                 color: Color.fromRGBO(37, 40, 73, 1),
+               ),
+             children: [
+               TextSpan(
+                  text: "и",
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Color.fromRGBO(252, 221, 61, 1),
+                  ),
+              children: [
+                TextSpan(
+                   text: "нтересных мест",
+                   style: TextStyle(
+                     fontSize: 32,
+                     color: Color.fromRGBO(37, 40, 73, 1),
+                   ),
+                ),
+              ]),
+            ]),
+          ]),
           textAlign: TextAlign.left, // текст по левому краю
-          style: TextStyle(
-            fontSize: 32,
-            color: Color.fromRGBO(37, 40, 73, 1), // цвет текста по дизайну
-          ),
-      ),// заголовок
-      toolbarHeight: 100, // задаем высоту апп бару
-    ),
-   );
-  }
+        ),
+      ),
+    );
+  } 
 }
